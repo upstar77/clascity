5127adf2796737d7f7e77c352d9b22a6ee90dfa3
SAVY Mathieu <savy.mathieu@gmail.com>
2017-08-14 12:38:50 -0700

    auth working

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Add Warden helpers
+  config.include Warden::Test::Helpers, type: :feature
+  config.after(type: :feature) { Warden.test_reset! }
+
 end
