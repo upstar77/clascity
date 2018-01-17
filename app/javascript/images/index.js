8fee5133bc21bbf3bb1401a9b104d3f1516088b2
Jacky Vo <jackyinvn@gmail.com>
2018-01-17 17:28:23 +0700

    Update webpack

---
+++ app/javascript/images/index.js
@@ -0,0 +1,37 @@
+import './favicon.png';
+import './clascity-logo.svg';
+import './avatar_default.png';
+import './clascity-logomark-grey.svg';
+
+import './icon-footer-facebook.svg';
+import './icon-nav-calendar.svg';
+import './icon-menu-btn.svg';
+import './icon-nav-notification.svg';
+import './icon-footer-twitter.svg';
+import './image-userProfile-Alice-2.png';
+import './image-userProfile-Claudia.png';
+
+import './icon-arrow-down.svg';
+import './icon-plus.svg';
+import './icon-search-button.svg';
+import './icon-fileUpload.svg';
+import './icon-seach.svg';
+import './icon-arrow-up.svg';
+import './icon-tooltip.svg';
+import './icon-arrow-next-passive.svg';
+import './icon-secondLevel.svg';
+import './icon-tooltip-blue.svg';
+
+import './icon-arrow-right-large-orange.svg';
+import './icon-changes-percent-white.svg';
+import './icon-changes-dollar-grey.svg';
+import './icon-changes-dollar-white.svg';
+import './icon-changes-positive-grey.svg';
+import './icon-changes-positive-white.svg';
+import './icon-changes-negative-grey.svg';
+import './icon-changes-negative-white.svg';
+import './icon-arrow-left-large-orange.svg';
+import './icon-arrow-left-large.svg';
+import './icon-arrow-right-large.svg';
+import './icon-changes-percent-grey.svg';
+
--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -6,9 +6,13 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-import 'bootstrap'; // Popper is in assets/javascript/application.js
+import 'bootstrap';
+import 'jquery-ui';
+import 'owl.carousel';
+
+import 'images';
+import 'fonts';
 
 import '../styles/application.scss';
-
 import '../src/dispatcher';
 
